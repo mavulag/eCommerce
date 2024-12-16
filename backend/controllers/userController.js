f44ef9aae16cf49cd6bc734c35e0d69c44ffc0fa
Mavula Geofrey <83079451+mavulag@users.noreply.github.com>
2024-12-16 11:26:46 +0000

    Create userController

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,12 @@
 // Get user model
 import User from "../models/userModel";
 
+// Get asyncHandler
+import asyncHandler from "../middlewares/asyncHandler";
+
 // Start creating User
-const createUser = 
+const createUser = asyncHandler(async (req, res) => {
+  res.send("Hello");
+});
+
+export { createUser };
