657ded4e26c5cfa604f8416f9a6e333bb1c7255b
Mavula Geofrey <83079451+mavulag@users.noreply.github.com>
2024-12-16 06:49:10 +0000

    Testing index.js file if reflect any changes of the codes through the console.log() function

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,12 @@
 // Import packages
-import path from 'path';
-import express from 'express';
-import dotenv from 'dotenv';
-import cookieParser from 'cookie-parser';
+import path from "path";
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 // Utils
 // console.log('Importing connectDB from:', './config/db.js');
-import connectDB from './config/db.js';
+import connectDB from "./config/db.js";
 
 // Load environment variables from .env file
 dotenv.config();
@@ -30,8 +30,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 // Define a simple route that responds with "Hello World!"
-app.get('/', (req, res) => {
-    res.send("Hello World!");
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
 // Start the server and listen on the specified port
